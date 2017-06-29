1cbd6b748a906b320abc6aa4c4280975cda70be5
Arthur Loza <loza.uns@gmail.com>
2017-06-29 23:42:49 +0300

    HW7_JSd: init

---
+++ dom.js
@@ -0,0 +1,82 @@
+/**
+ * removeAllSpans -  функция, которая удаляет все элементы span со страницы
+ */
+
+function removeAllSpans () {
+	// TODO
+}
+
+
+/**
+ * firstChild -  функция, которая будет кроссбразурено возвращать firstChild (не текстовую ноду).
+ *
+ * @param {Node} parent node
+ * @return {Node}
+ */
+
+function firstChild (parent) {
+
+}
+
+
+/**
+ * lastChild -  функция, будет кроссбраузерно возвращать lastChild (не текстовую ноду).
+ *
+ * @param {Node} parent node
+ * @return {Node}
+ */
+
+function lastChild (parent) {
+
+}
+
+
+/**
+ * next -  функция, которая будет кроссбраузерно возвращать следующий элемент
+ * (не текстовую ноду, работа наподобие next() в jQuery).
+ *
+ * @param {Node} node
+ * @return {Node}
+ */
+
+function next (node) {
+
+}
+
+
+/**
+ * prev -  функция, которая будет кроссбраузерно возвращать предыдущий элемент
+ * (не текстовую ноду, работа наподобие prev() в jQuery).
+ *
+ * @param {Node} node
+ * @return {Node}
+ */
+
+function prev (node) {
+
+}
+
+
+/**
+ * OPTIONAL
+ * closest -  функция, которая будет идти вверх по DOM (по родительским елементам) от переданного ей элемента в первом
+ * аргументе пока не найдет элемент переданный во втором аргументе в эту функцию и вернёт этот элемент (то есть второй).
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+ *
+ * Example:
+ * closest(document.querySelector('[data-url="http://google.ru"]'), 'aside'); // return <aside>...</aside>
+ *
+ * @param {Node} node
+ * @param {String} selector
+ * @return {Node}
+ */
+
+function closest (node, selector) {
+
+}
+
+
+
+
+
+
